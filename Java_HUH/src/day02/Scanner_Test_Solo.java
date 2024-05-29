82789098b8528108921afbc2f311feba402d8b31
cocoparoma <bnbz201@naver.com>
2024-05-29 17:33:15 +0900

    3일차 혼자해본 스케너 복습, 다중연산항
    if를 쓰는게 좋을듯 하다

---
+++ Java_HUH/src/day02/Scanner_Test_Solo.java
@@ -0,0 +1,17 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Scanner_Test_Solo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 1 : ");
+		int a = sc.nextInt();
+		System.out.print("정수 2: ");
+		int b = sc.nextInt();
+		System.out.print("연산자 : ");	
+		char operator = sc.next().charAt(0);
+		System.out.println((operator == '+') ? ""+a+operator+b+"="+(a+b) : (operator == '-') ? ""+a+operator+b+"="+(a-b) : (operator == '*') ? ""+a+operator+b+"="+(a*b): (operator == '/') ? ""+a+operator+b+"="+(a/b):"잘못 입력하셨습니다.");
+	}
+}
