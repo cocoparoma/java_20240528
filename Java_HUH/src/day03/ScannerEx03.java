610fa7a8193bd480133decc2cf31e5ed8c673e07
cocoparoma <bnbz201@naver.com>
2024-05-30 12:49:10 +0900

    자바 4일차 스캐너 예제3

---
+++ Java_HUH/src/day03/ScannerEx03.java
@@ -0,0 +1,16 @@
+package day03;
+import java.util.Scanner;
+public class ScannerEx03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score1, score2, score3;
+		System.out.println("세 학생의 성적을 입력하세요 : ");
+		score1 = sc.nextInt();
+		score2 = sc.nextInt();
+		score3 = sc.nextInt();
+		System.out.println("세 학생의 성적은 " + score1 + ", " + score2 + ", " + score3 + "입니다.");
+		//스캐너 사용시 일자 길을 잘 생각해보자(공백 즉 \n 구분하기 위해서)
+	
+	}
+
+}
