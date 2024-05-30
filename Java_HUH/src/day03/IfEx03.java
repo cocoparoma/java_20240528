63aef09cb4c65ea960c1dafa5463e9ef5dccfefd
cocoparoma <bnbz201@naver.com>
2024-05-30 12:48:33 +0900

    자바 4일차 if문 예제3

---
+++ Java_HUH/src/day03/IfEx03.java
@@ -0,0 +1,18 @@
+package day03;
+import java.util.Scanner;
+public class IfEx03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//else if Ex
+		System.out.print("정수를 입력해 주세요 : ");
+		int num = sc.nextInt();
+		if(num>0) {
+			System.out.println("입력하신 수는 양수입니다.");
+		}else if(num == 0) {
+			System.out.println("입력하신 수는 0입니다.");
+		}else {
+			System.out.println("입력하신 수는 음수입니다.");
+		}
+		
+	}
+}
