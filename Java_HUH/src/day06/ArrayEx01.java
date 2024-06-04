02829100ad00f0c4afe5077f3f47340aa6647ea2
cocoparoma <bnbz201@naver.com>
2024-06-04 17:11:38 +0900

    자바 6일차 배열공부 01

---
+++ Java_HUH/src/day06/ArrayEx01.java
@@ -0,0 +1,23 @@
+package day06;
+
+public class ArrayEx01 {
+
+	public static void main(String[] args) {
+		int[] arr1, arr2;
+		int arr3[], arr4;
+		
+		arr1 = new int[3]; //arr1 = array
+		arr2 = new int[3];//arr2 = array
+		arr3 = new int[3];//arr3 = array
+		arr4 = 10;//arr4 != array // arr4 = var
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
