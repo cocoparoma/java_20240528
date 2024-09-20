78b9d0f9636ed068e678573d4bfe68a87569ca47
cocoparoma <bnbz201@naver.com>
2024-09-20 11:30:54 +0900

    타일즈, DB연동

--- spring/spring_Study2/src/main/java/kr/kh/study2/config/WebMvcConfig.java
+++ spring/spring_Study2/src/main/java/kr/kh/study2/config/WebMvcConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -24,5 +26,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+    
+ // TilesViewResolver 설정
+    @Bean
+    public TilesViewResolver tilesViewResolver() {
+        TilesViewResolver tilesViewResolver = new TilesViewResolver();
+        tilesViewResolver.setOrder(1); // ViewResolver의 우선순위 설정
+        return tilesViewResolver;
+    }
+    // Tiles 설정
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions("/WEB-INF/spring/tiles.xml");
+        tilesConfigurer.setCheckRefresh(true); // 변경 사항을 자동으로 감지하여 갱신
+        return tilesConfigurer;
     }
 }
---
+++ spring/spring_Study2/src/main/java/kr/kh/study2/controller/UserController.java
@@ -0,0 +1,5 @@
+package kr.kh.study2.controller;
+
+public class UserController {
+
+}
