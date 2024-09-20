a14773ef1f213869f519d35e1ba067536df352b9
cocoparoma <bnbz201@naver.com>
2024-09-20 14:38:19 +0900

    gmail 보내기 + config 나누기, 관리가 용이함

--- spring/spring_Study2/src/main/java/kr/kh/study2/controller/HomeController.java
+++ spring/spring_Study2/src/main/java/kr/kh/study2/controller/HomeController.java
@@ -1,6 +1,10 @@
 package kr.kh.study2.controller;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +22,9 @@ public class HomeController {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
+	@Autowired
+	private JavaMailSender mailSender;
+	
 	
 	@GetMapping("/")
 	public String home(Model mo) {
@@ -32,6 +39,9 @@ public class HomeController {
 		System.out.println("암호화 된 문자열 : " + enc);
 		System.out.println("암호화 된 문자열 일치 여부(abc) :" + passwordEncoder.matches("abc", enc));
 		System.out.println("암호화 된 문자열 일치 여부(test) :" + passwordEncoder.matches("test", enc));
+		
+		//메일전송 테스트
+//		mailSend("bnbz201@naver.com", "메일 테스트", "전송이 잘 되었습니다.");//이메일 아이디와 비번을 안적어서 테스트 안됨
 		
 		return "/home";
 	}
@@ -59,7 +69,26 @@ public class HomeController {
 	}
 	
 	
-	
+	public boolean mailSend(String to, String title, String content) {
+
+	    String setfrom = "stajun@naver.com"; //의미 없으나 필요함
+	   try{
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper messageHelper
+	            = new MimeMessageHelper(message, true, "UTF-8");
+
+	        messageHelper.setFrom(setfrom);// 보내는사람 생략하거나 하면 정상작동을 안함
+	        messageHelper.setTo(to);// 받는사람 이메일
+	        messageHelper.setSubject(title);// 메일제목은 생략이 가능하다
+	        messageHelper.setText(content, true);// 메일 내용
+
+	        mailSender.send(message);
+	        return true;
+	    } catch(Exception e){
+	        e.printStackTrace();
+	        return false;
+	    }
+	}
 	
 	
 	
