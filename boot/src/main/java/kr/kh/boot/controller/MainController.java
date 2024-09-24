47e9177407b8141f1a982055789d6167eba75db7
cocoparoma <bnbz201@naver.com>
2024-09-24 16:56:43 +0900

    스프링 부트 1일차

--- boot/src/main/java/kr/kh/boot/controller/MainController.java
+++ boot/src/main/java/kr/kh/boot/controller/MainController.java
@@ -1,14 +1,38 @@
 package kr.kh.boot.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+import kr.kh.boot.dao.PostDAO;
+
+
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private PostDAO postDao;
+	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model mo, HttpSession session) {
 		
-		return "/index";
+		mo.addAttribute("count", postDao.count());
+		mo.addAttribute("sid", session.getId());
+		mo.addAttribute("name", "홍길동");
+		return "index";
+	}
+
+
+	@GetMapping("/index2")
+	public String home2(Model mo, HttpSession session) {
+			
+		mo.addAttribute("sid", session.getId());
+		mo.addAttribute("name", "홍길동");
+		return "index2";
 	}
 }
