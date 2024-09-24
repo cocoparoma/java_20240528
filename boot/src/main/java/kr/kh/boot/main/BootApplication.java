d83beb43c51ddd6ae79b2749f55469d6ebf0baf5
cocoparoma <bnbz201@naver.com>
2024-09-24 15:23:44 +0900

    기본 설정 적용

--- boot/src/main/java/kr/kh/boot/main/BootApplication.java
+++ boot/src/main/java/kr/kh/boot/main/BootApplication.java
@@ -1,4 +1,4 @@
-package kr.kh.boot;
+package kr.kh.boot.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- boot/src/main/java/kr/kh/boot/main/ServletInitializer.java
+++ boot/src/main/java/kr/kh/boot/main/ServletInitializer.java
@@ -1,4 +1,4 @@
-package kr.kh.boot;
+package kr.kh.boot.main;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
